54ea2d17aa49b27222ae63f62c52f5dfc627ad02
Francisco Noriega <frannoriega.92@gmail.com>
2016-08-22 14:05:35 -0300

    EJ1 codeado

--- TP1/Ejercicios/ej1.cpp
+++ TP1/Ejercicios/ej1.cpp
@@ -1,4 +1,6 @@
-#include <bits/stdc++.h>
+//#include <bits/stdc++.h>
+#include <vector>
+#include <iostream>
 
 typedef long long ll;
 #define forr(i,a,b) for(int i=(a); i<(b); ++i)
@@ -6,13 +8,43 @@ typedef long long ll;
 
 using namespace std;
 
-int main(int argc, char const *argv[]) {
+void generateParts(vector<int>& v, int num){
+  int size = v.size();
+  v.push_back(num);
+  forn(i, size){
+     v.push_back(num+v[i]);
+  }
+}
+
+int getMax(vector<int>& first, vector<int>& second, int limit){
+  int max = 0;
+  int j = second.size()-1;
+  forn(i, first.size()){
+    while(first[i] + second[j] > limit && j > 0) j-=1;
+    int sum = first[i]+second[j];
+    if(sum > limit) return max;
+    if(sum > max) max = sum;
+  }
+  return max;
+}
+
+int main() {
   int P, N;
   cin >> P >> N;
-  vector<int> packs(N);
-  forn(i, N)
-    cin >> packs[i];
-
+  int half = N/2;
+  vector<int> fpacks(half);
+  vector<int> spacks(N-half);
+  forn(i, N){
+    int num;
+    cin >> num;
+    if(i < half)
+      generateParts(fpacks, num);
+    else
+      generateParts(spacks, num);
+  }
+  sort(fpacks.begin(), fpacks.end());
+  sort(spacks.begin(), spacks.end());
+  cout << getMax(fpacks, spacks, P) << endl;
   return 0;
 }
 
