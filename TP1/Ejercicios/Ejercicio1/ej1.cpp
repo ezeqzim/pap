272a631e3a32c548dd1ac5622597376214bbc109
Francisco Noriega <you@example.com>
2016-08-27 20:29:12 -0300

    Cambie el push_back en ej1

--- TP1/Ejercicios/Ejercicio1/ej1.cpp
+++ TP1/Ejercicios/Ejercicio1/ej1.cpp
@@ -6,13 +6,22 @@ typedef long long ll;
 
 using namespace std;
 
-void generateParts(vector<ll>& v, int num){
-  int size = v.size();
-  v.push_back(num);
-  forn(i, size){
-     v.push_back(num+v[i]);
+void generateParts(vector<ll>& v, int num, int iter){
+  int size = (1<<iter)-1; //La posicion donde agregar el nuevo elemento (o sea, desde 0..size esta todo lo que fui calculando)
+  v[size] = num;
+  forn(i, size){ //Recorro todo lo que ya calcule
+     v[size+i+1] = num+v[i]; //Y lo guardo en las posiciones siguientes
   }
 }
+
+//OLD GENERATEPARTS
+//void generateParts(vector<ll>& v, int num){
+//  int size = v.size();
+//  v.push_back(num);
+//  forn(i, size){
+//    v.push_back(num+v[i]);
+//  }
+//}
 
 int getMax(vector<ll>& first, vector<ll>& second, int limit){
   int max = 0, sum;
@@ -30,15 +39,15 @@ int main() {
   int P, N;
   cin >> P >> N;
   int half = N/2;
-  vector<ll> fpacks;
-  vector<ll> spacks;
+  vector<ll> fpacks(1<<half); //Me guardo 2^(n/2) en cada arreglo, para guardar las partes
+  vector<ll> spacks(1<<(N-half));
   int num;
   forn(i, N){
     cin >> num;
     if(i < half)
-      generateParts(fpacks, num);
+      generateParts(fpacks, num, i);//Paso el i como parametro para saber en que iteracion estoy
     else
-      generateParts(spacks, num);
+      generateParts(spacks, num, i-half);
   }
   fpacks.push_back(0);
   spacks.push_back(0);
