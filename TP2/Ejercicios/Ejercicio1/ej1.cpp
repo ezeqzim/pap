30d44d5b5a1030ee90c9eb4d18af8f0e8e732de5
Ezequiel Zimenspitz <ezeqzim@gmail.com>
2016-09-23 02:45:57 -0300

    edmons karp

--- TP2/Ejercicios/Ejercicio1/ej1.cpp
+++ TP2/Ejercicios/Ejercicio1/ej1.cpp
@@ -1,88 +1,118 @@
 #include "../stdc++.h"
 
+using namespace std;
+
 typedef long long ll;
-typedef std::pair<int, int> eje;
 typedef int nodo;
 #define forr(i,a,b) for(int i=(a); i<(b); ++i)
 #define forn(i,n) forr(i,0,n)
 
-using namespace std;
+vector<vector<nodo> > grafo;
+nodo fuente, sumidero;
 
-vector<list<nodo> > grafo;
-int m, n;
-char c;
-nodo sumidero;
-nodo fuente;
-
+int edmondsKarp(){
+  int n = grafo.size(), f = 0, capacity = 1;
+  vector<int> p(n), m(n);
+  vector<vector<int> > flujo(n, vector<int>(n, 0));
+  while(true){
+    forn(i, n)
+      p[i] = -1;
+    p[fuente] = -2;
+    m[fuente] = 1000000000;
+    m[sumidero] = 0;
+    queue<nodo> q;
+    q.push(fuente);
+    bool b = true;
+    while(!q.empty() && b){
+      nodo v = q.front();
+      q.pop();
+      forn(i, grafo[v].size()){
+        nodo vec = grafo[v][i];
+        if(capacity > flujo[v][vec] && p[vec] == -1){
+          p[vec] = v;
+          m[vec] = min(m[v], capacity - flujo[v][vec]);
+          q.push(vec);
+          if(vec == sumidero){
+            b = false;
+            break;
+          }
+        }
+      }
+    }
+    if(b)
+      break;
+    f += m[sumidero];
+    nodo v = sumidero;
+    while(v != fuente){
+      flujo[p[v]][v] += m[sumidero];
+      flujo[v][p[v]] -= m[sumidero];
+      v = p[v];
+    }
+  }
+  return f;
+}
 
 nodo out(nodo v) {
-    return 2*v+1;
+  return 2*v+1;
 }
 
 nodo in(nodo v) {
-    return 2*v;
+  return 2*v;
 }
 
-void test(){
-    // Este test toma el grafo nuevo, osea, con fuente y sumidero y con los nodos partidos en v_in y v_out y 
-    // reconstruye el grafo original más fuente y sumidero. Es decir, muestra como estaban conectados los 
-    // nodos originales y a qué nodos se conecta la fuente. Es para ver si se armó bien, ésta función se
-    // puede eliminar más adelante.
-
-    forr(i,0,n+1){
-        cout << "Nodo: ";
-        if (i == 0) cout << "Fuente" << endl;
-        else cout << i << endl;
-        cout << "Ejes salientes a: ";
-        for (nodo v : grafo[out(i)]) {
-            if (v == fuente) cout << "fuente ";
-            else if (v==sumidero) cout << "sumidero ";
-            else cout << v/2 << " ";
-        }
-        cout << endl;
+void test(int n){
+  // Este test toma el grafo nuevo, osea, con fuente y sumidero y con los nodos partidos en v_in y v_out y
+  // reconstruye el grafo original más fuente y sumidero. Es decir, muestra como estaban conectados los
+  // nodos originales y a qué nodos se conecta la fuente. Es para ver si se armó bien, ésta función se
+  // puede eliminar más adelante.
+  forn(i, n+1){
+    cout << "Nodo: ";
+    if(i == 0) cout << "Fuente" << endl;
+    else cout << i << endl;
+    cout << "Ejes salientes a: ";
+    forn(j, grafo[out(i)].size()) {
+      if(grafo[out(i)][j] == fuente) cout << "fuente ";
+      else if(grafo[out(i)][j] == sumidero) cout << "sumidero ";
+      else cout << grafo[out(i)][j]/2 << " ";
     }
+    cout << endl;
+  }
 }
 
-void armar_nodos(){
-	forr(i,1,n+1){
-    	grafo[in(i)].push_back(out(i));   // Conecto todos los u.in con u.out
-    	cin >> c;
-    	if (c == 'A') {   // De la fuente voy a los alumnos
-    		grafo[fuente].push_back(in(i));
-    	}
-    	else if (c == 'E') {  // De las escuelas voy al sumidero
-    		grafo[out(i)].push_back(sumidero);
-    	}
-    }
+void armar_nodos(int n){
+  char c;
+  forr(i, 1, n+1){
+    grafo[in(i)].push_back(out(i)); // Conecto todos los u.in con u.out
+    cin >> c;
+    if(c == 'A') // De la fuente voy a los alumnos
+      grafo[fuente].push_back(in(i));
+    else if(c == 'E') // De las escuelas voy al sumidero
+      grafo[out(i)].push_back(sumidero);
+  }
 }
 
-void armar_ejes(){
-	int u,v;
-	forn(i,m){
-    	cin >> u >> v;     // Si u y v están conectados, entonces de u.out sale eje a v.in y de v.out a u.in
-    	grafo[out(u)].push_back(in(v));
-    	grafo[out(v)].push_back(in(u));
-    }
+void armar_ejes(int m){
+  int u,v;
+  forn(i, m){
+    cin >> u >> v; // Si u y v están conectados, entonces de u.out sale eje a v.in y de v.out a u.in
+    grafo[out(u)].push_back(in(v));
+    grafo[out(v)].push_back(in(u));
+  }
 }
-
 
 int main(int argc, char const *argv[]) {
-
-	// Fuente = 1
-	// Sumidero = 0
-	// Para toda esquina i, esquina_i_in = 2i
-	// Para toda esquina i, esquina_i_out = 2i+1
-
-	cin >> n >> m;
-	grafo = vector<list<nodo>>(2*n+2);
-	sumidero = 0;
-	fuente = 1;
-
-	armar_nodos();
-	armar_ejes();
-    test();
-   
-
-
-    return 0;
+  // Fuente = 1
+  // Sumidero = 0
+  // Para toda esquina i, esquina_i_in = 2i
+  // Para toda esquina i, esquina_i_out = 2i+1
+  int n, m;
+  cin >> n >> m;
+  grafo = vector<vector<nodo> >(2*n+2);
+  sumidero = 0;
+  fuente = 1;
+  armar_nodos(n);
+  armar_ejes(m);
+  cout << edmondsKarp() << endl;
+  // test(n);
+  return 0;
 }
