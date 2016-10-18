312df23d9fe5dce4b4e2db8568c94c4937b1f8bd
nchamo <nicolas@chamo.com.ar>
2016-10-18 20:32:29 -0300

    Correcion ejercicio 3

--- TP3/Ejercicios/Ejercicio3/ej3.cpp
+++ TP3/Ejercicios/Ejercicio3/ej3.cpp
@@ -7,19 +7,17 @@ typedef long long ll;
 using namespace std;
 
 int c, a, q;
-int c1, a1, c2, a2;
-int res;
 vector<vector<int> > memo;
 
 void inicializar() {
   cin >> c >> a >> q;
-  memo = vector<vector<int> >(c+1, vector<int>(a+1, 0));
+  memo = vector<vector<int> >(c + 1, vector<int>(a + 1, 0));
 }
 
 void armar_memo() {
-  forr(i, 1, c+1) {
-    forr(j, 1, a+1) {
-      int val;
+  int val;
+  forr(i, 1, c + 1) {
+    forr(j, 1, a + 1) {
       cin >> val;
       memo[i][j] = val + memo[i - 1][j] + memo[i][j - 1] - memo[i - 1][j - 1];
     }
@@ -27,10 +25,10 @@ void armar_memo() {
 }
 
 void resolver_queries(){
+  int c1, a1, c2, a2;
   forn(i, q) {
     cin >> c1 >> a1 >> c2 >> a2;
-    res = memo[c2][a2] + memo[c1][a1] - memo[c2][a1] - memo[c1][a2];
-    cout << res << endl;
+    cout << (memo[c2][a2] + memo[c1][a1] - memo[c2][a1] - memo[c1][a2]) << endl;
   }
 }
 
