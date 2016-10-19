ccdea93613cc9634460bf4f377ace5502606dfc2
Francisco Noriega <frannoriega.92@gmail.com>
2016-10-19 17:30:14 -0300

    Ej2 'listo'. Faltan tests

--- TP3/Ejercicios/Ejercicio2/ej2.cpp
+++ TP3/Ejercicios/Ejercicio2/ej2.cpp
@@ -6,6 +6,82 @@ typedef long long ll;
 
 using namespace std;
 
+struct Node{
+	//bool endOfWord;
+	int maxAmountOfWords;
+	map<char,Node> next;
+	Node() : /*endOfWord(false), */maxAmountOfWords(0), next() {};
+	/*bool*/void addWord(string& s, int index, int size){
+		if(index == size){
+		//	if(endOfWord) return false;
+		//	else{
+		//		endOfWord = true;
+		//		return endOfWord;
+		//	}
+			return;
+		}
+		//else{
+
+			map<char,Node>::iterator it;
+			it = next.find(s[index]);
+			//bool res;
+			if(it != next.end()){
+				/*res = */(*it).second.addWord(s,index+1,size);		
+			}
+			else{
+				Node nextNode;
+				/*res = */nextNode.addWord(s,index+1,size);
+				next.insert(pair<char,Node>(s[index],nextNode));
+			}
+			//if(res){
+				maxAmountOfWords++;
+			//}
+			//return res;
+		//}
+	};
+	int getMaxAmountOfWords(){
+		return maxAmountOfWords;
+	};
+};
+
+class Trie{
+	public:
+		Trie() : next(), maxAmountOfWords(0) {}
+		void updateMaxAmountOfWords(Node& n){
+			maxAmountOfWords = max(n.getMaxAmountOfWords(), maxAmountOfWords);
+		}
+		void addWord(string& s, int index, int size){
+			map<char,Node>::iterator it;
+			it = next.find(s[index]);
+			if(it != next.end()){
+				(*it).second.addWord(s,index+1,size);
+				updateMaxAmountOfWords((*it).second);
+			}
+			else{
+				Node nextNode;
+				nextNode.addWord(s,index+1,size);
+				next.insert(pair<char,Node>(s[index],nextNode));
+				updateMaxAmountOfWords(nextNode);
+			}
+		}
+		int getMaxAmountOfWords(){
+			return maxAmountOfWords;
+		}
+	private:
+		map<char,Node> next;
+		int maxAmountOfWords;
+};
+
 int main(int argc, char const *argv[]) {
-  return 0;
+	int students;
+	cin >> students;
+	Trie diccionary;
+	for(int i = 0; i < students; i++){
+		string mail;
+		int prefix;
+		cin >> mail >> prefix;
+		diccionary.addWord(mail,0,prefix);
+	}
+	cout << diccionary.getMaxAmountOfWords() << endl;
+	return 0;
 }
