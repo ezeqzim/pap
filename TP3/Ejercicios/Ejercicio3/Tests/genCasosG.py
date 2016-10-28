fa2c47b3500709b9c81e5b0cb06f0db57a3c857e
Ezequiel Zimenspitz <ezeqzim@gmail.com>
2016-10-28 16:25:08 -0300

    test grandes ej3. puse ll y extraje res a una variable ll para que se haga todo bien

--- TP3/Ejercicios/Ejercicio3/Tests/genCasosG.py
+++ TP3/Ejercicios/Ejercicio3/Tests/genCasosG.py
@@ -11,11 +11,31 @@ if(len(sys.argv) == 1):
 else:
   filename = sys.argv[1] + ".big"
   with open(filename,"w") as f:
-    N = r.randint(1, 10**5)
-    f.write(str(N))
+    A = r.randint(1, 10**3)
+    C = r.randint(1, 10**3)
+    Q = r.randint(1, 10**5)
+    f.write(str(A))
+    f.write(" ")
+    f.write(str(C))
+    f.write(" ")
+    f.write(str(Q))
     f.write("\n")
-    for i in range(0, N):
-      f.write(str(r.randint(0, 10**9)))
-      if(i != N-1):
-        f.write(" ")
-    f.write("\n")
+    for i in range(0, A):
+      for i in range(0, C):
+        f.write(str(r.randint(0, 10**9)))
+        if(i != C-1):
+          f.write(" ")
+      f.write("\n")
+    for i in range(0, Q):
+      a1 = r.randint(1, A-1)
+      a2 = r.randint(a1, A)
+      c1 = r.randint(1, C-1)
+      c2 = r.randint(c1, C)
+      f.write(str(a1))
+      f.write(" ")
+      f.write(str(c1))
+      f.write(" ")
+      f.write(str(a2))
+      f.write(" ")
+      f.write(str(c2))
+      f.write("\n")
