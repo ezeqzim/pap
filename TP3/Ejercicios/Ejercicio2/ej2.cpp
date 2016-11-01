d365dff03c7d232c273f8224e2fdce6d4c227576
Ezequiel Zimenspitz <ezeqzim@gmail.com>
2016-10-31 23:48:41 -0300

    making sense

--- TP3/Ejercicios/Ejercicio2/ej2.cpp
+++ TP3/Ejercicios/Ejercicio2/ej2.cpp
@@ -7,72 +7,69 @@ typedef long long ll;
 using namespace std;
 
 struct Node{
-	public:
-		Node() : maxAmountOfWords(0), next() {};
+  public:
+    Node() : maxAmountOfWords(0), next() {};
 
-		void addWord(string& s, int size, int index = 0){
-			if(index < size){
-				if(index == size - 1)
-					finished = true;
-				map<char, Node>::iterator it = next.find(s[index]);
-				if(it != next.end())
-					it->second.addWord(s, size, index + 1);
-				else{
-					Node nextNode;
-					nextNode.addWord(s, size, index + 1);
-					next.insert(pair<char,Node>(s[index], nextNode));
-				}
-			}
-			maxAmountOfWords++;
-		};
+    void addWord(string& s, int size, int index = 0){
+      if(index == size)
+        finished = true;
+      else if(index < size){
+        map<char, Node>::iterator it = next.find(s[index]);
+        if(it != next.end())
+          it->second.addWord(s, size, index + 1);
+        else{
+          Node nextNode;
+          nextNode.addWord(s, size, index + 1);
+          next.insert(pair<char,Node>(s[index], nextNode));
+        }
+      }
+      maxAmountOfWords++;
+    };
 
-		int getMaxAmountOfWordsFromPrefix(){
-			int maximum = 0;
-			for(map<char, Node>::iterator it = next.begin(); it != next.end(); ++it){
-				if(finished)
-					maximum = max(maximum, it->second.maxAmountOfWords);
-				maximum = max(maximum, it->second.getMaxAmountOfWordsFromPrefix());
-			}
-			return maximum;
-		};
+    int getMaxAmountOfWordsFromPrefix(){
+      int maximum = finished ? maxAmountOfWords : 0;
+      for(map<char, Node>::iterator it = next.begin(); it != next.end(); ++it)
+        maximum = max(maximum, it->second.getMaxAmountOfWordsFromPrefix());
+      return maximum;
+    };
 
-	private:
-		int maxAmountOfWords;
-		bool finished = false;
-		map<char, Node> next;
+  private:
+    int maxAmountOfWords;
+    bool finished = false;
+    map<char, Node> next;
 };
 
 class Trie{
-	public:
-		Trie() : root() {};
+  public:
+    Trie() : root() {};
 
-		void addWord(string& s, int size){
-			root.addWord(s, size);
-		}
+    void addWord(string& s, int size){
+      root.addWord(s, size);
+    }
 
-		int getMaxAmountOfWordsFromPrefix(){
-			root.getMaxAmountOfWordsFromPrefix();
-		}
+    int getMaxAmountOfWordsFromPrefix(){
+      root.getMaxAmountOfWordsFromPrefix();
+    }
 
-	private:
-		Node root;
+  private:
+    Node root;
 };
 
 
 void initialize(Trie& dictionary){
-	int students;
-	cin >> students;
-	for(int i = 0; i < students; i++){
-		string mail;
-		int prefix;
-		cin >> mail >> prefix;
-		dictionary.addWord(mail, prefix);
-	}
+  int students;
+  cin >> students;
+  for(int i = 0; i < students; i++){
+    string mail;
+    int prefix;
+    cin >> mail >> prefix;
+    dictionary.addWord(mail, prefix);
+  }
 }
 
 int main(int argc, char const *argv[]) {
-	Trie dictionary;
-	initialize(dictionary);
-	cout << dictionary.getMaxAmountOfWordsFromPrefix() << endl;
-	return 0;
+  Trie dictionary;
+  initialize(dictionary);
+  cout << dictionary.getMaxAmountOfWordsFromPrefix() << endl;
+  return 0;
 }
